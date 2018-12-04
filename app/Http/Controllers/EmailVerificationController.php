557aeb47ab63bc0b24f5694be05c8a63bc96353d
DorisYang <doris@DorisYangdeMacBook-Pro.local>
2018-12-04 23:18:55 +0800

    异常处理

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\InvalidRequestException;
 use Exception;
 use App\Models\User;
 use Cache;
@@ -18,13 +19,13 @@ class EmailVerificationController extends Controller
         $token=$request->input('token');
         //参数判断
         if(!$email || !$token){
-            throw new Exception('验证链接不正确');
+            throw new InvalidRequestException('验证链接不正确');
         }
 
         //从缓存中取数据，与获取的‘token’值对比 是否一致
 
         if($token != Cache::get('email_verification_'.$email)){
-            throw new Exception('验证链接不正确或已过期');
+            throw new InvalidRequestException('验证链接不正确或已过期');
         }
 
         // 根据邮箱从数据库中获取对应的用户
@@ -32,7 +33,7 @@ class EmailVerificationController extends Controller
         // 但是为了代码的健壮性我们还是需要做这个判断
 
         if(!$user= User::where('email',$email)->first()){
-            throw new Exception('用户不存在');
+            throw new InvalidRequestException('用户不存在');
         }
 
         // 将指定的 key 从缓存中删除，由于已经完成了验证，这个缓存就没有必要继续保留。
@@ -50,7 +51,7 @@ class EmailVerificationController extends Controller
         $user=$request->user();
         //判断用户是否已验证
         if($user->email_verified){
-            throw new Exception('用户已验证');
+            throw new InvalidRequestException('用户已验证');
         }
 
         //调用notify() 方法来发送定义好的通知类
