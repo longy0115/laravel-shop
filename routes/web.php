be7e08298c224b4a99e031550b590cc37f8f7a90
DorisYang <doris@DorisYangdeMacBook-Pro.local>
2018-12-06 17:50:42 +0800

    管理后台 - 商品详情

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::group(['middleware' => 'auth'], function() {
 
 Route::redirect('/', '/products')->name('root');
 Route::get('products', 'ProductsController@index')->name('products.index');
-
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
