4b2d94db28a16341568d8ab97ef0d6128550b953
DorisYang <doris@DorisYangdeMacBook-Pro.local>
2018-12-12 15:58:19 +0800

    管理后台 - 拒绝退款

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -104,6 +104,12 @@
             <button type="button" id="btn-receive" class="btn btn-sm btn-success">确认收货</button>
           </div>
           @endif
+          @if(isset($order->extra['refund_disagree_reason']))
+          <div>
+            <span>拒绝退款理由：</span>
+            <div class="value">{{ $order->extra['refund_disagree_reason'] }}</div>
+          </div>
+          @endif
           <!-- 订单已支付，且退款状态是未退款时展示申请退款按钮 -->
           @if($order->paid_at && $order->refund_status === \App\Models\Order::REFUND_STATUS_PENDING)
           <div class="refund-button">
@@ -111,7 +117,9 @@
           </div>
           @endif
         </div>
+        
       </div>
+        
     </div>
   </div>
 </div>
